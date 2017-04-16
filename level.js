d44329c8e8408c082c64052547f5c2ffc8def2b6
Luna <judahiii@gmail.com>
2017-04-16 03:21:46 -0400

    Fix text not updating sometimes; slight refactor
    Some things that really should be in created were not, they were moved,
    and an if was unneeded so aaaall those lines got marked as new (which
    sucks!)

--- level.js
+++ level.js
@@ -25,111 +25,109 @@ Level.prototype.makeCracks = function(cracks)
 
 Level.prototype.draw = function()
 {
+
 	var storedStage = levels[this.storedLevel];
 
-	var cracks = 0.2;
+	var cracks = 0.5;
 
-	if (this.isNew)
+	storedStage.addChild(game.make.sprite(0, 0, "floor"));
+
+	this.walls = storedStage.addChild(game.make.group());
+	this.walls.enableBody = true;
+	var wall = this.walls.create(0, 0);
+	wall.body.width = 320;
+	wall.body.height = 26;
+	wall.body.immovable = true;
+	wall = this.walls.create(0, 0);
+	wall.body.width = 3;
+	wall.body.height = 200;
+	wall.body.immovable = true;
+	wall = this.walls.create(317, 0);
+	wall.body.width = 3;
+	wall.body.height = 200;
+	wall.body.immovable = true;
+	wall = this.walls.create(0, 180 - 3);
+	wall.body.width = 320;
+	wall.body.height = 3;
+	wall.body.immovable = true;
+
+	this.cracks = storedStage.addChild(game.make.group());
+
+	var levelText = game.make.retroFont("arabic", 12, 16, Phaser.RetroFont.TEXT_SET1);
+	storedStage.addChild(game.make.image(225, 4, levelText));
+	levelText.text = "Level:" + this.storedLevel;
+
+	this.sorted = storedStage.addChild(game.make.group());
+
+	this.monsters = storedStage.addChild(game.make.group());
+	this.monsters.enableBody = true;
+	this.monsters.classType = Monster;
+
+	this.makeCracks(Math.random() * this.storedLevel * cracks - 1);
+
+	for (var i=0; i<Math.random() * this.storedLevel * cracks; i++)
 	{
-
-		storedStage.addChild(game.make.sprite(0, 0, "floor"));
-
-		this.walls = storedStage.addChild(game.make.group());
-		this.walls.enableBody = true;
-		var wall = this.walls.create(0, 0);
-		wall.body.width = 320;
-		wall.body.height = 26;
-		wall.body.immovable = true;
-		wall = this.walls.create(0, 0);
-		wall.body.width = 3;
-		wall.body.height = 200;
-		wall.body.immovable = true;
-		wall = this.walls.create(317, 0);
-		wall.body.width = 3;
-		wall.body.height = 200;
-		wall.body.immovable = true;
-		wall = this.walls.create(0, 180 - 3);
-		wall.body.width = 320;
-		wall.body.height = 3;
-		wall.body.immovable = true;
-
-		this.cracks = storedStage.addChild(game.make.group());
-
-		this.sorted = storedStage.addChild(game.make.group());
-
-		this.monsters = storedStage.addChild(game.make.group());
-		this.monsters.enableBody = true;
-		this.monsters.classType = Monster;
-
-		this.makeCracks(Math.random() * this.storedLevel * cracks - 1);
-
-		for (var i=0; i<Math.random() * this.storedLevel * cracks; i++)
-		{
-			var monster = this.monsters.create(0, 0);
-		}
-
-		var portalLocations = [[80, 60],  [160, 60],  [240, 60],
-			[80, 120], [160, 120], [240, 120]];
-		// Pick a portal and remove it from the list
-		var up = portalLocations.splice(Math.floor(Math.random() * 6), 1)[0];
-		this.portalUp = storedStage.addChild(game.make.sprite(up[0], up[1] - 24, "portalUp"));
-		game.physics.arcade.enable(this.portalUp);
-		this.portalUp.body.offset = new Phaser.Point(0, 17);
-		this.portalUp.body.width = 25;
-		this.portalUp.body.height = 15;
-		this.portalUp.body.immovable = true;
-		// Prevent from going to levels[0]
-		if (this.storedLevel != 1)
-		{
-			// Pick a portal from the reduced list
-			var down = portalLocations[Math.floor(Math.random() * 5)];
-			this.portalDown = storedStage.addChild(game.make.sprite(down[0], down[1] - 24, "portalDown"));
-			game.physics.arcade.enable(this.portalDown);
-			this.portalDown.body.offset = new Phaser.Point(0, 17);
-			this.portalDown.body.width = 25;
-			this.portalDown.body.height = 15;
-			this.portalDown.body.immovable = true;
-		}
-
-		this.player = storedStage.addChild(new Player(this.portalUp, this.portalDown));
-
-		this.bullets = storedStage.addChild(game.make.group());
-		this.enemyBullets = storedStage.addChild(game.make.group());
-
-		storedStage.addChild(game.make.sprite(0, 180 - 26, "frontWall"));
-
-		this.font = game.make.retroFont("arabic", 12, 16, Phaser.RetroFont.TEXT_SET1);
-		storedStage.addChild(game.make.image(5, 180 - 22, this.font));
-
-		// Add objects that exist in isometric space to a group that gets sorted
-		this.sorted.add(this.monsters);
-		this.sorted.add(this.player);
-		this.sorted.add(this.portalUp);
-		if (this.portalDown)
-		{
-			this.sorted.add(this.portalDown);
-		}
-
-		if (game.device.touch)
-		{
-			storedStage.addChild(game.make.sprite(25,  110, "dpad"));
-			storedStage.addChild(game.make.sprite(250, 110, "dpad"));
-		}
-
+		var monster = this.monsters.create(0, 0);
 	}
 
-	this.makeCracks(Math.random() * cracks * (entropy - this.lastEntropy) - 1);
-	this.lastEntropy = entropy;
+	var portalLocations = [[80, 60],  [160, 60],  [240, 60],
+		[80, 120], [160, 120], [240, 120]];
+	// Pick a portal and remove it from the list
+	var up = portalLocations.splice(Math.floor(Math.random() * 6), 1)[0];
+	this.portalUp = storedStage.addChild(game.make.sprite(up[0], up[1] - 24, "portalUp"));
+	game.physics.arcade.enable(this.portalUp);
+	this.portalUp.body.offset = new Phaser.Point(0, 17);
+	this.portalUp.body.width = 25;
+	this.portalUp.body.height = 15;
+	this.portalUp.body.immovable = true;
+	// Prevent from going to levels[0]
+	if (this.storedLevel != 1)
+	{
+		// Pick a portal from the reduced list
+		var down = portalLocations[Math.floor(Math.random() * 5)];
+		this.portalDown = storedStage.addChild(game.make.sprite(down[0], down[1] - 24, "portalDown"));
+		game.physics.arcade.enable(this.portalDown);
+		this.portalDown.body.offset = new Phaser.Point(0, 17);
+		this.portalDown.body.width = 25;
+		this.portalDown.body.height = 15;
+		this.portalDown.body.immovable = true;
+	}
 
-	this.entropyText();
+	this.player = storedStage.addChild(new Player(this.portalUp, this.portalDown));
 
-	this.isNew = false;
+	this.bullets = storedStage.addChild(game.make.group());
+	this.enemyBullets = storedStage.addChild(game.make.group());
+
+	storedStage.addChild(game.make.sprite(0, 180 - 26, "frontWall"));
+
+	this.font = game.make.retroFont("arabic", 12, 16, Phaser.RetroFont.TEXT_SET1);
+	storedStage.addChild(game.make.image(5, 180 - 22, this.font));
+
+	// Add objects that exist in isometric space to a group that gets sorted
+	this.sorted.add(this.monsters);
+	this.sorted.add(this.player);
+	this.sorted.add(this.portalUp);
+	if (this.portalDown)
+	{
+		this.sorted.add(this.portalDown);
+	}
+
+	if (game.device.touch)
+	{
+		storedStage.addChild(game.make.sprite(25,  110, "dpad"));
+		storedStage.addChild(game.make.sprite(250, 110, "dpad"));
+	}
 
 }
 
+// Called evry time the level is set
 Level.prototype.create = function()
 {
 	game.stage = levels[this.storedLevel];
+	var cracks = 0.2;
+	this.makeCracks(Math.random() * cracks * (entropy - this.lastEntropy) - 1);
+	this.lastEntropy = entropy;
+	this.entropyText();
 }
 
 // Default direction is 50/50 -1, 1
